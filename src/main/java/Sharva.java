7517f04adb78f9b98632fdf16a11647462d3c8d6
Sahishnu <sahishnuk2@gmail.com>
2025-08-17 22:38:09 +0800

    Add support for echo

--- src/main/java/Sharva.java
+++ src/main/java/Sharva.java
@@ -1,12 +1,29 @@
+import java.util.Scanner;
+
 public class Sharva {
+    private static final String horizontalLine = "    __________________________________________";
+
+    public static void sayHello() {
+        System.out.println(horizontalLine);
+        System.out.println("    Hello! I'm Sharva\n    What can I do for you?");
+        System.out.println(horizontalLine);
+    }
+
+    public static void echo(String userInput) {
+        System.out.println(horizontalLine);
+        System.out.println(userInput.equals("bye") ? "    Bye. Hope to see you again soon!" : "    " + userInput);
+        System.out.println(horizontalLine);
+    }
+
     public static void main(String[] args) {
-        String horizontalLine = "__________________________________________";
-        String sayHello = "Hello! I'm Sharva\nWhat can I do for you?";
-        String sayBye = "Bye. Hope to see you again soon!";
-        System.out.println(horizontalLine);
-        System.out.println(sayHello);
-        System.out.println(horizontalLine);
-        System.out.println(sayBye);
-        System.out.println(horizontalLine);
+        sayHello();
+        Scanner scanner = new Scanner(System.in);
+        String curr = scanner.nextLine();
+        while (!curr.equals("bye")) {
+            echo(curr);
+            curr = scanner.nextLine();
+        }
+        echo("bye");
+        scanner.close();
     }
 }
