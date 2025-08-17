a43e4e2e9c24cebbf9a231788ac0b591d5b84a87
Sahishnu <sahishnuk2@gmail.com>
2025-08-17 22:17:42 +0800

    Add greeting

--- src/main/java/Sharva.java
+++ src/main/java/Sharva.java
@@ -1,10 +1,12 @@
 public class Sharva {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String horizontalLine = "__________________________________________";
+        String sayHello = "Hello! I'm Sharva\nWhat can I do for you?";
+        String sayBye = "Bye. Hope to see you again soon!";
+        System.out.println(horizontalLine);
+        System.out.println(sayHello);
+        System.out.println(horizontalLine);
+        System.out.println(sayBye);
+        System.out.println(horizontalLine);
     }
 }
